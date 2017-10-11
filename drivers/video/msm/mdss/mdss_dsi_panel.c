2aaf43e757fbaa1143949b6b2ba6d7d6f9fb8391
energyspear17 <energyspear17@gmail.com>
2017-10-11 07:39:58 +0530

    Implement Lazyplug with LCD Notifier

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -58,6 +58,10 @@ static char rc_range_max_qp[] = {4, 4, 5, 6, 7, 7, 7, 8, 9, 10, 11, 12,
 static char rc_range_bpg_offset[] = {2, 0, 0, -2, -4, -6, -8, -8, -8, -10, -10,
 	-12, -12, -12, -12};
 
+#ifdef CONFIG_LAZYPLUG
+extern void lazyplug_enter_lazy(bool enter);
+#endif
+
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	if (ctrl->pwm_pmi)
@@ -751,6 +755,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 	display_on = true;
 
+	#ifdef CONFIG_LAZYPLUG
+		lazyplug_enter_lazy(false); 
+	#endif
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -854,6 +862,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	}
 
 	display_on = false;
+
+	#ifdef CONFIG_LAZYPLUG
+		lazyplug_enter_lazy(true);
+	#endif
 
 end:
 	pr_debug("%s:-\n", __func__);
